f0b8dedc97f6342c54505a975b08e016cc9dd207
JeffESchmitz <jeff.schmitz@realpage.com>
2016-03-03 07:57:16 -0600

    Adding the swipe delete to the SentMemes table view

--- MemeMe/SentMemesTableViewController.swift
+++ MemeMe/SentMemesTableViewController.swift
@@ -43,25 +43,22 @@ class SentMemesTableViewController: UITableViewController {
         navigationController?.pushViewController(detailController, animated: true)
     }
 
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
+            // Delete the current meme (indexPath.row) from the meme collection
+            appDelegate.memes.removeAtIndex(indexPath.row)
+            
             // Delete the row from the data source
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
     /*
     // Override to support rearranging the table view.
